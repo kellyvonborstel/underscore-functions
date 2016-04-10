4f6406c065c33167c7dddac4c3e1d086ef27c7d4
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-04-10 14:38:05 -0700

    implement _.identity through _.indexOf

--- src/underbar.js
+++ src/underbar.js
@@ -3,66 +3,51 @@
 
   window._ = {};
 
-  // Returns whatever value is passed as the argument. This function doesn't
-  // seem very useful, but remember it--if a function needs to provide an
-  // iterator when the user does not pass one in, this will be handy.
+  // Returns whatever value is passed as the argument.
+  // Use this if a function needs an iterator when the user does not pass one.
   _.identity = function(val) {
+    return val;
   };
 
-  /**
-   * COLLECTIONS
-   * ===========
-   *
-   * In this section, we'll have a look at functions that operate on collections
-   * of values; in JavaScript, a 'collection' is something that can contain a
-   * number of values--either an array or an object.
-   *
-   *
-   * IMPORTANT NOTE!
-   * ===========
-   *
-   * The .first function is implemented for you, to help guide you toward success
-   * in your work on the following functions. Whenever you see a portion of the
-   * assignment pre-completed, be sure to read and understand it fully before
-   * you proceed. Skipping this step will lead to considerably more difficulty
-   * implementing the sections you are responsible for.
-   */
-
-  // Return an array of the first n elements of an array. If n is undefined,
-  // return just the first element.
   _.first = function(array, n) {
     return n === undefined ? array[0] : array.slice(0, n);
   };
 
-  // Like first, but for the last elements. If n is undefined, return just the
-  // last element.
+  // Returns an array of the last n elements of an array. If n is undefined,
+  // return just the last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    }
+    return n === undefined ? array[array.length - 1] : array.slice(-n);
   };
 
-  // Call iterator(value, key, collection) for each element of collection.
+  // Calls iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
-  //
-  // Note: _.each does not have a return value, but rather simply runs the
-  // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
-    // TIP: Here's an example of a function that needs to iterate, which we've
-    // implemented for you. Instead of using a standard `for` loop, though,
-    // it uses the iteration helper `each`, which you will need to write.
     var result = -1;
-
-    _.each(array, function(item, index) {
-      if (item === target && result === -1) {
+    _.each(array, function(value, index) {
+      if (value === target && result === -1) {
         result = index;
-      }
+      }      
     });
-
     return result;
   };
+
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
